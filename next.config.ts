9be072e1f6169228d97311549b68b23fca93570c
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2026-04-16 15:25:40 -0400

    change build to next js default

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-//   output: 'export', // Outputs a Single-Page Application (SPA)
-  distDir: 'build', // Changes the build output directory to `build`
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
